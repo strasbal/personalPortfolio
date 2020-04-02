55876a6284de783e0f2d81eb06396d56c1bd4fef
strasbal <strasbal@gmail.com>
2020-04-02 11:06:55 -0600

    reduce function not working yet

--- congress/index.js
+++ congress/index.js
@@ -8,18 +8,21 @@ const filterSenators = (prop, value) => {
     return senators.filter(senator => senator[prop] === value)
 }
 
-const mappedSenators = senators.map(senator => {
+function simplifiedSenators(SenatorArray) {
+
+senators.map(senator => {
     let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
     return {
         id: senator.id,
         name: `${senator.first_name}${middleName}${senator.last_name}`,
         imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
+        seniority: parseInt(senator.seniority, 10)
     }
 })
+}
 
 function populateContainer(smallSenatorsArray) {
-    console.log(smallSenatorsArray)
-    smallSenatorsArray.forEach(senator => {
+   return smallSenatorsArray.forEach(senator => {
         
         let senFigure = document.createElement('figure')
         let figImg = document.createElement('img')
@@ -34,4 +37,15 @@ function populateContainer(smallSenatorsArray) {
     })
 }
 
-populateContainer(mappedSenators)
+const republicans = filterSenators('party', 'R')
+const democrats = filterSenators('party', 'D')
+
+console.log(republicans)
+
+const mostSeniority = simplifiedSenators(republicans).reduce{
+    (acc, senator) => {
+        return acc.seniority > senator.seniority ? acc : senator
+    }, {}
+}
+
+populateContainer(simplifiedSenators(republicans))
