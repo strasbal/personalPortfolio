ee30db486f8996515af0f5a87ce113f0787519a5
strasbal <strasbal@gmail.com>
2020-04-05 21:52:57 -0600

    updated based on thortek

--- congress/index.js
+++ congress/index.js
@@ -1,4 +1,4 @@
-import { senators } from '../congress/data/senators.js'
+import { senators } from '../data/senators.js'
 
 // this is all about filter, map, reduce
 
@@ -8,21 +8,20 @@ const filterSenators = (prop, value) => {
     return senators.filter(senator => senator[prop] === value)
 }
 
-function simplifiedSenators(SenatorArray) {
-
-senators.map(senator => {
-    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
-    return {
-        id: senator.id,
-        name: `${senator.first_name}${middleName}${senator.last_name}`,
-        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
-        seniority: parseInt(senator.seniority, 10)
-    }
-})
+function simplifiedSenators(senatorArray) {
+    return senatorArray.map(senator => {
+        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+        return {
+            id: senator.id,
+            name: `${senator.first_name}${middleName}${senator.last_name}`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
+            seniority: parseInt(senator.seniority, 10)
+        }
+    })
 }
 
 function populateContainer(smallSenatorsArray) {
-   return smallSenatorsArray.forEach(senator => {
+    return smallSenatorsArray.forEach(senator => {
         
         let senFigure = document.createElement('figure')
         let figImg = document.createElement('img')
@@ -40,12 +39,12 @@ function populateContainer(smallSenatorsArray) {
 const republicans = filterSenators('party', 'R')
 const democrats = filterSenators('party', 'D')
 
-console.log(republicans)
-
-const mostSeniority = simplifiedSenators(republicans).reduce{
+const mostSeniority = simplifiedSenators(republicans).reduce(
     (acc, senator) => {
         return acc.seniority > senator.seniority ? acc : senator
-    }, {}
-}
+    }
+)
+
+console.log(mostSeniority)
 
 populateContainer(simplifiedSenators(republicans))
